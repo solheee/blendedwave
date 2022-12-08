42ec67930c83020c8a3b57ac2aa7d92bb0d483d2
solheee <09merus@gmail.com>
2022-12-08 17:43:59 +0900

    아티스트 페이지 틀

--- src/page/Artist.js
+++ src/page/Artist.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 import NavComponent from '../component/Nav';
@@ -12,22 +12,89 @@ import logo from '../styles/images/main_logo.svg';
 const Artist = styled.div`
   box-sizing: border-box;
   z-index: -99;
-  &>div {
+  padding-top: 58px;
+  min-height: 100vh;
+`
+
+const ArtistCard = styled.div`
 	display: flex;
-	justify-content: center;
-	align-items: center;
-	min-height: 100vh;
-  }
+	padding: 50px;
+	width: 80%;
+	background-color: #fff;
+	/* border-bottom: 2px solid #000; */
+	cursor: pointer;
+	>.artist-main-image {
+		min-width: 180px;
+		height: 180px;
+		margin-right: 50px;
+		background-color: #D9D9D9;
+	}
+	>.artist-info {
+		width: 100%;
+		>div:first-child {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			.artist-name {
+				font-size: 20px;
+			}
+			.artist-job {
+				font-size: 14px;
+			}
+		}
+		>div:nth-child(2) {
+			color: #DFDFDF;
+		}
+	}
 `
 
 const ArtistComponent = () => {
+	const [artistList, setArtistList] = useState([]);
+
+	useEffect(() => {
+		setArtistList([
+			{
+				name: '',
+				job: '',
+				sns: [
+					{
+						sns_name: 'discord',
+						sns_url: ''
+					}
+				],
+
+			},
+			{},
+			{}
+		])
+
+	}, [])
+
 
 	return (
 		<>
 			<NavComponent />
 			<Artist>
 				<div>
-					Artist
+					{artistList && artistList.map((item) => {
+						return (
+							<ArtistCard>
+								<div className='artist-main-image'></div>
+								<div className='artist-info'>
+									<div>
+										<div>
+											<p className='artist-name'>DISCUZZ</p>
+											<p className='artist-job'>Illustrator & composer</p>
+										</div>
+										<div>sns</div>
+									</div>
+									<div>
+										<p>discuzz@gmail.com</p>
+									</div>
+								</div>
+							</ArtistCard>
+						)
+					})}
 				</div>
 			</Artist>
 			<BottomComponent />
